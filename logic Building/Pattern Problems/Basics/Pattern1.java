fa61a8fcc1d923162c5a7aff05eb6930167f3d62
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-05-01 22:43:50 +0530

    Add files via upload

---
+++ logic Building/Pattern Problems/Basics/Pattern1.java
@@ -0,0 +1,23 @@
+package PatternProblems.Basics;
+
+public class Pattern1 {
+    public static void main(String [] arg){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Question1. Print the pattern shown below.
+
+/*
+****
+****
+****
+****
+****
+
+*/
